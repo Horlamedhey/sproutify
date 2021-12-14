30a275be00673b0929ad5b350ce3072bb50220d6
AbdulGafar Olamide Ajao /vary-able <Gafarolamide.ajao@gmail.com>
2021-12-14 07:27:09 +0100

    -Initial commit

---
+++ scripts/swiper.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const mySwiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  loop: true,
+  pagination: '.swiper-pagination',
+  grabCursor: true,
+  speed: 1000,
+  paginationClickable: true,
+  parallax: true,
+  autoplay: false,
+  effect: 'slide',
+  mousewheelControl: 1,
+})
---
+++ scripts/vue.js
@@ -0,0 +1,46 @@
+new Vue({
+  el: "#app",
+  data: {
+    timelines: [
+      {
+        date: "Q4 2020",
+        items: [
+          "Concept Development",
+          "Website Design",
+          "Sproutify Contract Development",
+          "Code Deployment on Testnet",
+        ],
+      },
+      {
+        date: "Q1 2021",
+        items: [
+          "Website Launch",
+          "Community Building",
+          "Community Engagement",
+          "External Developer Audit",
+          "Presale Planning",
+          "Uniswap Listing",
+          "Governance Launch",
+        ],
+      },
+      {
+        date: "Q2 2021",
+        items: [
+          "Liquidity Amplifier",
+          "Protocol Audit",
+          "Partnerships",
+          "Marketing",
+        ],
+      },
+      {
+        date: "Q3 2021",
+        items: [
+          "Continous Innovations and Protocol Development",
+          "Protocol Audit",
+          "Partnerships",
+          "Marketing",
+        ],
+      },
+    ],
+  },
+});
